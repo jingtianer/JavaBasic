c0800d26cd24848537851d1deba925d32aa5ed55
jingtianer <1043672911@qq.com>
2024-01-16 15:02:12 +0800

    stream

--- src/multithreading/CompletableFeatureDemo.java
+++ src/multithreading/CompletableFeatureDemo.java
@@ -1,2 +1,69 @@
-package multithreading;public class CompletableFeatureDemo {
+package multithreading;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CompletableFeatureDemo {
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+    public CompletableFuture<List<Integer>> getList(int len) {
+        return CompletableFuture.supplyAsync(()->{
+            Random random = new Random();
+            Integer[] array = new Integer[len];
+            for(int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(0, len);
+                System.out.println(Thread.currentThread().getName() + ", getList");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+            return new ArrayList<>(Arrays.asList(array));
+        }, executorService);
+    }
+    public  List<List<Integer>> toMatrix(List<Integer> a, List<Integer> b) {
+        List<List<Integer>> res = new ArrayList<>();
+        loop:
+        for (Integer x : a) {
+            ArrayList<Integer> line = new ArrayList<>();
+            for (Integer y : b) {
+                line.add(x + y);
+                System.out.println(Thread.currentThread().getName() + ", toMatrix");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    break loop;
+                }
+            }
+            res.add(line);
+        }
+        return res;
+    }
+    public <T> void printMatrix(List<List<T>> matrix) {
+        System.out.println(matrix);
+        executorService.shutdown();
+    }
+    void run() {
+        var future1 = CompletableFuture
+                .completedFuture(10)
+                .thenComposeAsync(this::getList, executorService);
+        var future2 = CompletableFuture
+                .completedFuture(20)
+                .thenComposeAsync(this::getList, executorService)
+                .thenCombine(future1, this::toMatrix)
+                .thenAccept(this::printMatrix);
+        while (!executorService.isShutdown()) {
+            System.out.println("do other work");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new CompletableFeatureDemo().run();
+    }
 }
--- src/stream/StreamDemo.java
+++ src/stream/StreamDemo.java
@@ -1,2 +1,68 @@
-package stream;public class StreamDemo {
+package stream;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamDemo {
+    private static <T> T with(T val, Consumer<T> consumer) {
+        if (val != null) {
+            consumer.accept(val);
+        }
+        return val;
+    }
+    final static Logger logger = Logger.getLogger(StreamDemo.class.getName());
+    private static <T> void dfs(T val, Function<T, Stream<T>> f) {
+        with(f.apply(val), (stream) -> {
+            stream.forEach(sub -> {
+                System.out.println(sub);
+                dfs(sub, f);
+            });
+        });
+    }
+    public static void main(String[] args) {
+        if(args.length < 2) return;
+        Path path = Path.of(args[0]);
+        Random random = new Random();
+        var randomStream = Stream.generate(random::nextDouble);
+        var sequenceStream = Stream.iterate(BigInteger.ONE, BigInteger.ONE::add);
+        var sequenceIte = sequenceStream.iterator();
+        var randomIte = randomStream.iterator();
+        dfs(Paths.get(args[1]), dir->{
+            if(dir == null) {
+                var rootDir = FileSystems.getDefault().getRootDirectories();
+                return StreamSupport.stream(rootDir.spliterator(), true);
+            } else {
+                var files = dir.toFile().listFiles();
+                if(files != null) {
+                    return StreamSupport.stream(
+                            Arrays.stream(files).map(File::toPath).spliterator(), true);
+                }
+            }
+            return null;
+        });
+
+        try (var lines = Files.lines(path)) {
+            lines.forEach(s -> {
+                String out = String.format("w:%s,%s:\n%s\n", randomIte.next(), sequenceIte.next().toString(), s);
+                System.out.println(out);
+            });
+        } catch (IOException e) {
+            logger.warning(e.toString());
+        }
+    }
 }
